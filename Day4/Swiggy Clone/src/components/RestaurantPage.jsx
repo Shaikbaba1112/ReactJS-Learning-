c152d6786bc4bed761d76ef47ee69b9e4de5f615
Shaikbaba1112 <shaikhbaba9539@gmail.com>
2026-02-17 16:55:43 +0530

    menu data fetched

--- Day4/Swiggy Clone/src/components/RestaurantPage.jsx
+++ Day4/Swiggy Clone/src/components/RestaurantPage.jsx
@@ -2,26 +2,37 @@ import { useEffect , useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const RestaurantPage = () => {
-    useEffect(()=>{
-        fetchRestaurantPage("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.3472352&lng=78.5083082&restaurantId=726147&catalog_qa=undefined&submitAction=ENTERY");
-    },[]);
+    const { resId } = useParams();
+    const [menuData, setMenuData] = useState(null);
 
-    async function fetchRestaurantPage(url){
+    useEffect(() => {
+        if (!resId) return;
+        console.log("Fetching menu data for restaurant ID:", resId);
+        fetchRestaurantPage(resId);
+    }, [resId]);
+
+    async function fetchRestaurantPage(restaurantId) {
         try {
+            const url = `https://proxy.corsfix.com/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.3472352&lng=78.5083082&restaurantId=${restaurantId}&catalog_qa=undefined&submitAction=ENTERY`;
             const rawData = await fetch(url);
-            console.log(rawData);
             const data = await rawData.json();
-            console.log(data);
+            console.log("Fetched menu data", data);
+            setMenuData(data);
         } catch (error) {
-            console.log(error);
+            console.log("Error while fetching menu data", error);
         }
     }
 
-    const useParamsValue = useParams();
-    console.log(useParamsValue.resId)
-    return(
-        <div style={{margin:"200px"}}>
-            Restaurant Name:{useParamsValue.resId}
+    return (
+        <div style={{ margin: "200px" }}>
+            <h2>Restaurant ID: {resId}</h2>
+            {menuData ? (
+                <div>
+                    <p>Menu fetched. Sections: {Object.keys(menuData).length}</p>
+                </div>
+            ) : (
+                <p>Loading menu...</p>
+            )}
         </div>
     );
-}
+};
